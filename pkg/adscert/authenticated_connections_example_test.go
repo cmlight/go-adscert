3f1ebd3d58eb32fec6013f7069263183c0b2cd13
Curtis Light <cmlight@gmail.com>
2021-04-26 16:35:26 +0000

    update demo signature string (that still doesn't work without stable PRNG)

--- pkg/adscert/authenticated_connections_example_test.go
+++ pkg/adscert/authenticated_connections_example_test.go
@@ -12,6 +12,7 @@ func ExampleAuthenticatedConnectionsSigner_SignAuthenticatedConnection() {
 	signatory := adscertcrypto.NewLocalAuthenticatedConnectionsSignatory("origin-signer.com", "a1b2c3")
 	signer := adscert.NewAuthenticatedConnectionsSigner(signatory)
 
+	// TODO: Add ability to seed PRNG for nonce and clock to generate deterministic results.
 	signatory.SynchronizeForTesting("destination-verifier.com")
 
 	// Determine the request parameters to sign.
@@ -28,7 +29,7 @@ func ExampleAuthenticatedConnectionsSigner_SignAuthenticatedConnection() {
 	}
 
 	fmt.Print("Signature passed via X-Ads-Cert-Auth: ", signature.SignatureMessages)
-	// Output: Signature passed via X-Ads-Cert-Auth: [from=origin-signer.com&invoking=example.com&status=0]
+	// Output: Signature passed via X-Ads-Cert-Auth: [from=origin-signer.com&from_key=a1b2c3&invoking=destination-verifier.com&nonce=ZRC3FNU3skLS&status=0&timestamp=210426T163109&to=destination-verifier.com&to_key=a1b2c3; sigb=HLIYY-dTGn6D&sigu=Sbe5OWsUlFXU]
 
 }
 
@@ -43,7 +44,7 @@ func ExampleAuthenticatedConnectionsSigner_VerifyAuthenticatedConnection() {
 	// TODO: assemble sample code to show this based on HTTP package.
 	destinationURL := "https://ads.origin-signer.com/request-ads"
 	body := []byte("{'id': '12345'}")
-	messageToVerify := "from=origin-signer.com&invoking=origin-signer.com&status=0; sigb=x&sigu=y"
+	messageToVerify := "from=origin-signer.com&from_key=a1b2c3&invoking=destination-verifier.com&nonce=ZRC3FNU3skLS&status=0&timestamp=210426T163109&to=destination-verifier.com&to_key=a1b2c3; sigb=HLIYY-dTGn6D&sigu=Sbe5OWsUlFXU"
 
 	verification, err := signer.VerifyAuthenticatedConnection(
 		adscert.AuthenticatedConnectionSignatureParams{
